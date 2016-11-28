7e3c78b4a87bed228b34b068ad3f9c666ca4909e
Artem Poletsky <artempoletsky@gmail.com>
2016-11-28 18:45:11 +0500

    DOM manipulations

---
+++ src/index.js
@@ -0,0 +1,5 @@
+$(function(){
+    $('#add_chart_button').on('click', function(e){
+        console.log($('#add_chart_input').val());
+    });
+});
--- src/no_framework.js
+++ src/no_framework.js
@@ -1,4 +1,44 @@
+$ = function (selector) {
+    if(typeof selector == 'function')
+    {
+        $.ready(selector)
+    }else
+    {
+        return new $__(selector);
+    }
+}
 
-$=function(selector){
-    return document.querySelectorAll(selector);
+$__ = function (selector) {
+    this.el = document.querySelectorAll(selector);
+}
+
+$.fn = $__.prototype;
+
+$.fn.on = function (event_name, callback) {
+    this.el[0].addEventListener(event_name, callback);
+    return this;
+}
+
+$.fn.val = function () {
+    if(arguments.length){
+        this.el[0].value = arguments[0];
+        return this;
+    }else{
+        return this.el[0].value;
+    }
+}
+
+$.get = function (url, callback) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(JSON.parse(this.responseText));
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+$.ready = function (callback) {
+    document.addEventListener("DOMContentLoaded", callback);
 }
