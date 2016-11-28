6067ebf12863dbd5b55f07a64f85f0a3bdf7847c
Artem Poletsky <artempoletsky@gmail.com>
2016-11-28 21:34:51 +0500

    continue vanilla jsing...

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-$(function(){
-    $('#add_chart_button').on('click', function(e){
-        console.log($('#add_chart_input').val());
+$(function () {
+    /*var getData = function (code, callback) {
+     var api_url = 'https://query.yahooapis.com/v1/public/yql';
+     var query = 'select * from yahoo.finance.quotes where symbol in ("' + code + '")';
+
+     $.get(api_url + '?q=' + query + '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', callback);
+
+     }*/
+
+    var getImageURL = function (code, period) {
+        return 'http://chart.finance.yahoo.com/z?s=' + code + '&t=' + period + '&q=l&l=on';
+    };
+    var $chartView = $('.chart_view').remove();
+    var $container = $('.chart_views');
+    $('#add_chart_button').on('click', function (e) {
+        var code = $('#add_chart_input').val();
+
+        var imageURL = getImageURL(code, '1d');
+        $chartView.find('.chart_image').attr('src', imageURL);
+        //console.log($chartView.find('.chart_image').el[0]);
+        $container.append($chartView.clone());
     });
 });
--- src/no_framework.js
+++ src/no_framework.js
@@ -1,32 +1,84 @@
 $ = function (selector) {
-    if(typeof selector == 'function')
-    {
+    if (typeof selector == 'function') {
         $.ready(selector)
-    }else
-    {
+    } else {
         return new $__(selector);
     }
-}
+};
 
 $__ = function (selector) {
-    this.el = document.querySelectorAll(selector);
-}
+    if (!selector) {
+        this.el = [];
+    } else if (typeof selector == 'string') {
+        this.el = $.toArray(document.querySelectorAll(selector))
+    } else {
+        this.el = $.toArray(selector);
+    }
+};
 
 $.fn = $__.prototype;
 
 $.fn.on = function (event_name, callback) {
     this.el[0].addEventListener(event_name, callback);
     return this;
-}
+};
+
+$.toArray = function (nodeList) {
+    return [].slice.call(nodeList);
+};
 
 $.fn.val = function () {
-    if(arguments.length){
+    if (arguments.length) {
         this.el[0].value = arguments[0];
         return this;
-    }else{
+    } else {
         return this.el[0].value;
     }
-}
+};
+
+$.fn.find = function (selector) {
+    var result = $();
+    this.el.forEach(function (el) {
+        result.el=result.el.concat($.toArray(el.querySelectorAll(selector)));
+    });
+    return result;
+};
+
+$.fn.attr = function (key, value) {
+    if (value !== undefined) {
+        this.el.forEach(function (el) {
+            el.setAttribute(key, value);
+        })
+    } else {
+        return el.getAttribute(key);
+    }
+    return this;
+};
+
+$.fn.clone = function () {
+    return $([this.el[0].cloneNode(true)]);
+};
+
+$.fn.append = function (elem) {
+    if (elem.el) {
+        elem = elem.el;
+    }
+    var parent = this.el[0];
+    if (!elem.length) {
+        elem = [elem];
+    }
+    for (var i = 0; i < elem.length; i++) {
+        parent.appendChild(elem[i]);
+    }
+    return this;
+};
+
+$.fn.remove = function () {
+    this.el.forEach(function (el) {
+        el.parentNode.removeChild(el);
+    });
+    return this;
+};
 
 $.get = function (url, callback) {
     var xhttp = new XMLHttpRequest();
@@ -37,8 +89,8 @@ $.get = function (url, callback) {
     };
     xhttp.open("GET", url, true);
     xhttp.send();
-}
+};
 
 $.ready = function (callback) {
     document.addEventListener("DOMContentLoaded", callback);
-}
+};
